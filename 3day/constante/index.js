95710b98507f89967de59101d1e24ca50a71c01d
Fernando Cavalcante <ftcavalcantee@gmail.com>
2024-09-16 10:59:14 -0300

    3 day using constant

---
+++ 3day/constante/index.js
@@ -0,0 +1,15 @@
+
+
+let name = "fernando Cavalacante"
+console.log(name);
+name = 'fernando gago'
+console.log(name)
+
+console.log('======================= CONSTANTE ===========')
+
+const nameConts = 'cavalcante' 
+console.log(nameConts);
+
+//NAO SE ALTERA O VALOR
+nameConts = 'tenorio' 
+console.log(nameConts)
