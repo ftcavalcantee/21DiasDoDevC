dddae942fc7ad2414881efd37637be57bb1178ff
Fernando Cavalcante <ftcavalcantee@gmail.com>
2024-09-16 12:41:18 -0300

    3 day using prompt

---
+++ 3day/prompt/index.js
@@ -0,0 +1,7 @@
+let receivingUserValue = prompt('coloque sua idade aqui')
+console.log(receivingUserValue)
+
+// parseInte = coverte de texto para numero
+
+let receivingUserValueEmInt = parseInt(receivingUserValue)
+console.log(receivingUserValueEmInt)
