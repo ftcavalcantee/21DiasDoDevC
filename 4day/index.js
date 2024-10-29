dd56615074623d8f06c2a45c0bb6a709c4343b3b
Fernando Cavalcante <ftcavalcantee@gmail.com>
2024-10-29 15:53:27 -0300

    atividade do 4day

---
+++ 4day/index.js
@@ -0,0 +1,15 @@
+
+// == igual / != diferente / < menor que e > maior que / <= >= maior e menor ou igual
+// === e igual o valor e o tipo / !== e diferente o valor e o tipo / && - E / || - ou x  
+
+
+
+
+let idade = Number(prompt(' insira sua idade '))
+
+if(idade <= 10 ){
+    console.log(idade)
+    console.log('voce tem menos ou tem 10 anos')
+}else{
+    console.log('voce tem mais de 10 anos')
+}
